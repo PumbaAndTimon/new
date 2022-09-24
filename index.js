093a01a2bd5e3cff2e7d1d2b9b80c04630394cd4
PumbaAndTimon <algir.s1996@gmail.com>
2022-09-24 12:45:17 +0300

    урок 1

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 console.log("hello world");
 const ourPlanetName = "earth";
 console.log(ourPlanetName);
-let Time = "12";
-console.log(Time);
+let age = 12;
+console.log(age);
 let colichestvoStatei = "20";
 console.log(colichestvoStatei);
 let Payment = true;
 console.log(Payment);
 const userName = "sergey gretsky";
 console.log(userName);
+const UserData = `имя пользователя ${userName}, возраст ${age} лет`;
+console.log(UserData);
+let unknow;
+unknow = "robochiken";
+console.log(unknow);
+const userpay = {
+  name: "ivan",
+  age: "312",
+};
+console.log(typeof userName);

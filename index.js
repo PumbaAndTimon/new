b61284191fdc8f118ae48547a147fda8499d9aa6
PumbaAndTimon <algir.s1996@gmail.com>
2022-10-01 12:46:24 +0300

    les 3

--- index.js
+++ index.js
@@ -1,38 +1,73 @@
-console.log("hello world");
-const ourPlanetName = "earth";
-console.log(ourPlanetName);
-let age = 12;
-console.log(age);
-let colichestvoStatei = "20";
-console.log(colichestvoStatei);
-let Payment = true;
-console.log(Payment);
-const userName = "sergey gretsky";
-console.log(userName);
-const UserData = `имя пользователя ${userName}, возраст ${age} лет`;
-console.log(UserData);
-let unknow;
-unknow = "robochiken";
-console.log(unknow);
-const userpay = {
-  name: "ivan",
-  age: "312",
-};
+// // console.log("hello world");
+// // const ourPlanetName = "earth";
+// // console.log(ourPlanetName);
+// // let age = 12;
+// // console.log(age);
+// // let colichestvoStatei = "20";
+// // console.log(colichestvoStatei);
+// // let Payment = true;
+// // console.log(Payment);
+// // const userName = "sergey gretsky";
+// // console.log(userName);
+// // const UserData = `имя пользователя ${userName}, возраст ${age} лет`;
+// // console.log(UserData);
+// // let unknow;
+// // unknow = "robochiken";
+// // console.log(unknow);
+// // const userpay = {
+// //   name: "ivan",
+// //   age: "312",
+// // };
 
-const myName = "sergey";
-const nameToMuch = Number(myName);
-console.log(nameToMuch);
+// const myName = "sergey";
+// const nameToMuch = Number(myName);
+// console.log(nameToMuch);
 
-console.log(typeof +"45");
-console.log(typeof Number("45"));
+// console.log(typeof +"45");
+// console.log(typeof Number("45"));
 
-console.log(2 == 3);
-console.log(2 == 2);
-console.log(2 !== 2);
-console.log(2 >= 2);
+// console.log(2 == 3);
+// console.log(2 == 2);
+// console.log(2 !== 2);
+// console.log(2 >= 2);
 
-console.log(2 + 6 + "3");
-console.log("2" + 3 + 3);
-console.log(2 + 3);
+// console.log(2 + 6 + "3");
+// console.log("2" + 3 + 3);
+// console.log(2 + 3);
 
-console.log(Boolean(2 > 3));
+// console.log(2 > 3);
+// console.log(3 == "3");
+// console.log(5 > 4);
+
+// console.log("ананас" > "яблоко");
+// console.log(undefined == null);
+// console.log(undefined === null);
+// console.log(null == "\nO\n");
+// console.log(null === +"\nO\n");
+// console.log("" + 1 + 0);
+// console.log("" - 1 + 0);
+// console.log(true + false);
+// console.log(6 / "3");
+// console.log("2" * "3");
+// console.log(4 + 5 + "px");
+// console.log("$" + 4 + 5);
+// console.log("4" - 2);
+// console.log("4px" - 2);
+// console.log(7 / 0);
+// console.log("-9" + 5);
+// console.log("-9" - 5);
+// console.log(null + 1);
+// console.log(undefined + 1);
+// console.log("\t\n" - 2);
+
+// const Towns = ["paris", "NN"];
+// console.log(Towns);
+// Towns[1] = "parisw";
+// console.log(Towns);
+
+const myFilms = ["film 1", "film2", "film3"];
+console.log(myFilms[0]);
+myFilms.push("film4");
+console.log(myFilms);
+const lastIndex = myFilms.length - 2;
+console.log(myFilms[lastIndex]);
